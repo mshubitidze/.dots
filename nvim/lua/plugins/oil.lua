1f9dbeb2afd067923e80ac2bf31af193cee660c0
Misho Shubitidze <misho@mshub.dev>
2024-12-24 03:52:36 +0400

    nvim

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,13 +1,9 @@
 return {
   'stevearc/oil.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   config = function()
     require('oil').setup {
       columns = { 'icon' },
-      keymaps = {
-        ['<C-h>'] = false,
-        ['<M-h>'] = 'actions.select_split',
-      },
       view_options = {
         show_hidden = true,
       },
