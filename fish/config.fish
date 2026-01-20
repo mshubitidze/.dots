78139b36ebd9d6b0e518c1cca58c1469eaa23df2
Misho Shubitidze <misho@mshub.dev>
2026-01-21 00:52:50 +0400

    up

--- fish/config.fish
+++ fish/config.fish
@@ -79,3 +79,6 @@ end
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 fish_add_path $HOME/.local/bin
+
+# Added by Antigravity
+fish_add_path /Users/m/.antigravity/antigravity/bin
